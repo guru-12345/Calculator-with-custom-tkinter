0a85d157fb6c3b6348f4a531402f8cfc64d4c735
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-05 12:54:10 +0530

    Update Building a Basic Calculator.py

--- Building a Basic Calculator.py
+++ Building a Basic Calculator.py
@@ -2,7 +2,7 @@
 #Task 1- Change the background color to "Dark"
 #Task 2- Change the size of the window to 320x450
 #Task 3- Add the tilte of the window to "Calculator"
-#Task 4- Remove the comment in the line 19 to make window size fixed.
+#Task 4- Remove the comment in the line 17 to make window size fixed.
 
 # Importing all necessary modules from customtkinter
 from customtkinter import *  
@@ -11,16 +11,16 @@ from customtkinter import *
 root = CTk()  
 
 # Setting the size of the window (Width x Height)
-root.geometry("320x450")  
+root.geometry("400x450")  
 
 # Disabling the option to resize the window (Fixes size)
-root.resizable(False, False)  
+#root.resizable(False, False)  
 
 # Setting the title of the window
-root.title('Calculator')  
+root.title(' ')  
 
 # Setting the appearance mode to "dark" (changes theme)
-set_appearance_mode("dark")  
+set_appearance_mode("light")  
 
 # Running the main event loop to keep the window open
 root.mainloop()
