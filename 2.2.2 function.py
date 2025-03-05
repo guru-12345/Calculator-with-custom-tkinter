83cc8e6be8fe7d2552c19151ee759a5ca656cc6d
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-05 16:45:10 +0530

    Create 2.2.2 function.py

---
+++ 2.2.2 function.py
@@ -0,0 +1,8 @@
+#Run and observe the output
+#Task 1- Change the function name to "message"
+
+def greet():
+    print("Hello, welcome to Python!")
+
+# Call the function to run it
+greet()  # This will print: Hello, welcome to Python!
