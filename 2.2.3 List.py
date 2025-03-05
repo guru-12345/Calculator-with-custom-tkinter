c4f3647a6e1bca9dd962ad2bf3c311f3e1067c8d
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-05 16:54:14 +0530

    Create 2.2.3 List.py

---
+++ 2.2.3 List.py
@@ -0,0 +1,8 @@
+#Task 1- Craete a list to store operators and print them 
+
+# A list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+
+# Print the list
+print(numbers)   # This will print: [1, 2, 3, 4, 5]
