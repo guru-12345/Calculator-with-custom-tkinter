36ce454466d889f54b6abfadd83eb5ae09c273df
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-05 16:17:40 +0530

    Create 2.2 function.py

---
+++ 2.2 function.py
@@ -0,0 +1,22 @@
+#Run  the code and observe.
+#Task 1- Fix the errors in the variables 
+
+from customtkinter import *
+
+# Constants for appearance
+#5button_color = "#21201F"
+#hover-color = "#4D4A48"
+#button size = 80
+
+
+# Setup window
+root = CTk()
+root.geometry("320x450")
+root.resizable(False, False)
+root.title('Calculator')
+set_appearance_mode("Dark")
+
+# Create result display
+result = CTkLabel(root, text="0", width=400, bg_color="grey",anchor="e", font=("arial", 60, "bold"))
+result.pack()
+root.mainloop()
