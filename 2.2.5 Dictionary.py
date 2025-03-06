ba36eea766a74214867744f8a07d6c62434d8390
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-06 17:14:12 +0530

    Create 2.2.5 Dictionary.py

---
+++ 2.2.5 Dictionary.py
@@ -0,0 +1,10 @@
+#Run the code and observe the output.
+#Task 1- Create a dictionary named "my_friends", where the keys are your friends' names and the values are their ages. 
+#Then, print the age of one friend
+
+
+# Create a dictionary with names as keys and ages as values
+people = {"Alice": 12, "Bob": 14, "Charlie": 13}
+
+# Access a value using a key
+print(people["Alice"])  # This will print 12, the age of Alice
