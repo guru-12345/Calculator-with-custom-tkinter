bfa8b96c27201b3a839e77eaa48e03d63aaaf6c3
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-06 13:20:07 +0530

    Create 2.2.5 Conditionals.py

---
+++ 2.2.5 Conditionals.py
@@ -0,0 +1,10 @@
+#Run the code and observe the output
+#Task 1- Write a code with below hint
+#1️⃣ First, create a variable to store the day (e.g., day = "Sunday").
+#2️⃣ Use an if-else condition to check if the day is "Sunday".
+#3️⃣ Print "No school! 🎉" if it's Sunday, otherwise print "Go to school! 📚".
+
+if 10 > 5:
+    print("10 is greater than 5")
+else:
+    print("5 is greater")
