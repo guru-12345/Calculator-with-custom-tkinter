c3add12abc19bad08feee0b248bb69b27e5f742b
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-09 22:59:51 +0530

    Update 3.2 Creating the frame for the buttons.py

--- 3.2 Creating the frame for the buttons.py
+++ 3.2 Creating the frame for the buttons.py
@@ -1,42 +1,38 @@
 #Run the code and observe the output
 #Task 1- Uncomment the line 34 and observe the change in the output.
 
-from customtkinter import *  # Importing CustomTkinter for modern UI styling
+"""The for loop helps arrange buttons in rows so they look organized in the calculator"""
 
-def create_calculator():
-    # Constants for appearance
-    BUTTON_COLOR = "#21201F"  # Button background color
-    HOVER_COLOR = "#4D4A48"   # Button hover color
-    BUTTON_SIZE = 80          # Default button size
+from customtkinter import *  
 
-    # Button configurations (layout of the calculator)
-    button_layout = [
-        ['+/-', 'C', 'Backspace'],  
-        ['7', '8', '9', 'X'],       
-        ['4', '5', '6', '-'],      
-        ['1', '2', '3', '+'],       
-        ['0', '.', '=', '/' ]        
-    ] 
-    
-    root = CTk()  
-    root.geometry("320x450") 
-    root.resizable(False, False)  
-    root.title('Calculator')  
-    set_appearance_mode("Dark")  
+# Constants for appearance
+BUTTON_COLOR = "#21201F"  
+HOVER_COLOR = "#4D4A48"   
+BUTTON_SIZE = 80          
 
-    result = CTkLabel(root, text="0", width=400, bg_color="grey", 
-                     anchor="e", font=("arial", 60, "bold"))  # Right-aligned text
-    result.pack()  
-    
-    #The loop creates a frame for each row of buttons in button_layout, grouping buttons together.
-    for row_buttons in button_layout: 
-        frame = CTkFrame(root)  #create a frame for each row
-        #frame.pack()
 
-    
-    return root
-    
-    
-# Create and display the calculator window
-calculator = create_calculator()
-calculator.mainloop()
+button_layout = [
+    ['+/-', 'C', 'Backspace'],  
+    ['7', '8', '9', 'X'],       
+    ['4', '5', '6', '-'],      
+    ['1', '2', '3', '+'],       
+    ['0', '.', '=', '/' ]        
+] 
+
+root = CTk()  
+root.geometry("320x450") 
+root.resizable(False, False)  
+root.title('Calculator')  
+set_appearance_mode("Dark")  
+
+result = CTkLabel(root, text="0", width=400, bg_color="grey", 
+                 anchor="e", font=("arial", 60, "bold"))  # Right-aligned text
+result.pack()  
+
+# The loop creates a frame for each row of buttons in button_layout, grouping buttons together.
+for row_buttons in button_layout: 
+    frame = CTkFrame(root)  # Create a frame for each row
+    #frame.pack()
+
+
+root.mainloop()
