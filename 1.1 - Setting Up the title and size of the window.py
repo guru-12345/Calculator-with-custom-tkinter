dc23e05a7ad420eee47238296906356605110469
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-13 12:01:20 +0530

    Update 1.1 - Setting Up the title and size of the window.py

--- 1.1 - Setting Up the title and size of the window.py
+++ 1.1 - Setting Up the title and size of the window.py
@@ -1,6 +1,8 @@
 #Run the code to observe the output.
-#Task 1- Change the size of the window to 320x450
-#Task 2- Add the title of the window to "Calculator"
+#Task 1- Uncomment the line - 20 and observe the output.
+#Task 2- Change the size of the window to 320x450
+#Task 3-Comment line 17 and observe the title of window(by default title of window is CTk).
+#Task 4- Now ,Uncomment line 17 & Add the title of the window to "Calculator"
 
 # Importing all necessary modules from customtkinter
 from customtkinter import *  
@@ -15,4 +17,4 @@ root.geometry("400x250")
 root.title(' ')  
 
 # Running the main event loop to keep the window open
-root.mainloop()
+#root.mainloop()
