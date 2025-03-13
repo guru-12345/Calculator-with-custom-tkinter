d707243b74da03737356e3362e76d2817f7c024f
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-13 13:03:12 +0530

    Update 2.1 adding the label.py

--- 2.1 adding the label.py
+++ 2.1 adding the label.py
@@ -1,31 +1,22 @@
 #Run the code to observe the output.
-#Task 1- uncomment the line 28 and observe output.
+#Task 1- Uncomment line 19 and observe the output.
 #Task 2- Make the label as wide as the window width.
 #Task 3- Change the text alignment of label to right.
-#Line 27 - # Creates a label to display the result with "0" as the starting text, grey background, 
-#large bold font, and anchor set to "w" (left-aligned)
 
-# Importing all necessary modules from customtkinter
+
 from customtkinter import *  
 
-# Creating the main application window (root)
 root = CTk()  
-
-# Setting the size of the window (Width x Height)
 root.geometry("320x450")  
-
-# Disabling the option to resize the window (Fixes size)
-#root.resizable(False, False)  
-
-# Setting the title of the window
+root.resizable(False, False)  
 root.title('Calculator')  
-
-# Setting the appearance mode to "dark" (changes theme)
 set_appearance_mode("light")  
 
-# Create result display
+'''The below code creates a result label to display the result with "0" as the starting text, grey as background colour, 
+bold font, and anchor set to "w" (left-aligned) '''
+
 result = CTkLabel(root, text="0", width=220, bg_color="grey", anchor="w", font=("arial", 60, "bold"))
-#result.pack()
+#result.pack() #pack() arranges widgets inside a window automatically.
 
 # Running the main event loop to keep the window open
 root.mainloop()
