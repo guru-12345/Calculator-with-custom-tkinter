4a6639476fa7ac57d7e877f4270301b91d66a594
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-13 12:40:12 +0530

    Update 1.2 - Resizing the window and setting up the theme.py

--- 1.2 - Resizing the window and setting up the theme.py
+++ 1.2 - Resizing the window and setting up the theme.py
@@ -1,24 +1,22 @@
 #Run the code to observe the output.
-#Task 1- Change the appearance mode to "Dark"
-#Task 2- Uncomment in the line 15 to make window size fixed.
-#
-# Importing all necessary modules from customtkinter
+#Try increasing the height and width of the output window.
+
+#Task 1- Uncomment line 15 to make window size fixed.
+#Task 2- Make the width resizable.
+#Task 3- Change the appearance mode to "Dark".
+
 from customtkinter import *  
 
-# Creating the main application window (root)
 root = CTk()  
 
-# Setting the size of the window (Width x Height)
 root.geometry("320x450")  
 
-# Disabling the option to resize the window (Fixes size)
+#It stops resizing the window's Width & Height.
 #root.resizable(False, False)  
 
-# Setting the title of the window
 root.title('Calculator')  
 
 # Setting the appearance mode to "dark" (changes theme)
 set_appearance_mode("light")  
 
-# Running the main event loop to keep the window open
 root.mainloop()
