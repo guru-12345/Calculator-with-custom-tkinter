a8759c5410e7070b7d73b0158e5a5f605747458e
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-15 10:13:56 +0530

    Create 3.2.1 - List Indexing.py

---
+++ 3.2.1 - List Indexing.py
@@ -0,0 +1,10 @@
+#Run and observe the output.
+#Task 1- Print the number 3 from the list.
+#Task 2- Print the last second number from the list.
+
+# A list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Print the list
+print(numbers[0])   # This will print the first element of the list
+print(numbers[-1])  # This will print the last element of the list
