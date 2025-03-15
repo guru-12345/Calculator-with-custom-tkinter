e4bff0b49627193db5c966d6180b17776f5f4cc3
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-15 10:18:02 +0530

    Create 3.2.2. Nested List.py

---
+++ 3.2.2. Nested List.py
@@ -0,0 +1,12 @@
+#Run the code and observe output
+#Task 1- Print the 'Dessert' from the nested list.
+
+# This is a nested list
+favorite_foods = [
+    ['Pizza', 'Main Dish', 5],  # List inside the main list
+    ['Ice Cream', 'Dessert', 4],  # Another list inside the main list
+    ['Apple', 'Fruit', 3]
+]
+
+# Printing the whole list
+print(favorite_foods)
