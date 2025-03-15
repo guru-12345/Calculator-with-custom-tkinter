f97894abaa68f9138090668d736f286c33f58882
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-15 11:02:48 +0530

    Update and rename 2.2.5 Loops.py to 4.1 For Loop.py

--- 4.1 For Loop.py
+++ 4.1 For Loop.py
@@ -1,5 +1,6 @@
 # Run the code and observe the output.  
 # Task 1: Modify the code to print numbers starting from 20 and ending at 30.  
 
+# Using a for loop with range(start, stop)
 for i in range(1, 11):  
     print(i)  
