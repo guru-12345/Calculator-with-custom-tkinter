dce64b777a5cf0f331e57ff0a1fe65648e76e061
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-16 21:18:06 +0530

    Update 5.3 Conditional Statements.py

--- 5.3 Conditional Statements.py
+++ 5.3 Conditional Statements.py
@@ -1,9 +1,12 @@
-#Run and observe the output.
-# Task 1- Change the value of `num` to a number less than 5 and observe the output.
+# Run and observe the output.
+# Task 1 - Change the value of `num` to a number less than 5 and observe the output.
+# Task 2 - Change the value of num to exactly 5 and see what happens.
 
 num = 10  
 
 if num > 5:  
     print("The number is greater than 5")  
+elif num == 5:  
+    print("The number is exactly 5")  
 else:  
-    print("The number is 5 or smaller")  
+    print("The number is smaller than 5")  
