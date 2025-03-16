039fd413efab9bf83c104aaa2a51c6c6fb04f842
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-16 20:51:37 +0530

    Update and rename 2.2.6 Conditionals.py to 5.3 Conditional Statements.py

---
+++ 5.3 Conditional Statements.py
@@ -0,0 +1,9 @@
+#Run and observe the output.
+# Task 1- Change the value of `num` to a number less than 5 and observe the output.
+
+num = 10  
+
+if num > 5:  
+    print("The number is greater than 5")  
+else:  
+    print("The number is 5 or smaller")  
