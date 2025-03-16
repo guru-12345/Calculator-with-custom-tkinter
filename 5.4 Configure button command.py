3d13ef5d7af3d933126394f9b29fd19240348e77
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-16 21:03:30 +0530

    Update and rename 4.1 configure button command.py to 5.4 Configure button command.py

---
+++ 5.4 Configure button command.py
@@ -0,0 +1,59 @@
+# Run the code and observe the output by clicking on each button
+#Task 1:- Update the if-else code from lines 32 to 37 to 
+
+'''result.configure(text=value if current == "0" else current + value)'''
+
+from customtkinter import *  
+
+root = CTk()  
+root.geometry("320x450")  
+root.resizable(False, False)  
+root.title('Calculator')  
+set_appearance_mode("Dark")  
+
+result = CTkLabel(root, text="0", width=400, bg_color="grey", 
+                 anchor="e", font=("arial", 60, "bold"))  
+result.pack()  
+
+button_color = "black"
+hover_color = "orange"
+button_size = 80       
+
+button_layout = [
+    ['+/-', 'C', 'Backspace'],  
+    ['7', '8', '9', 'X'],       
+    ['4', '5', '6', '-'],      
+    ['1', '2', '3', '+'],       
+    ['0', '.', '=', '/' ]        
+]
+
+special_widths = {'Backspace': 160}  
+
+def button_click(value):
+    current = result.cget("text")
+    if current == "0":  # If the current text is "0", replace it with the new value
+        result.configure(text=value)  # Update the label with the new value
+    else:  
+        result.configure(text=current + value)  # Append the new value to the existing text
+ 
+
+for row_buttons in button_layout:
+    frame = CTkFrame(root)
+    frame.pack()
+    
+    for button_text in row_buttons:
+        width = special_widths.get(button_text, button_size)
+        button = CTkButton(
+            frame,
+            text=button_text,
+            height=button_size,
+            width=width,
+            border_width=1,
+            hover_color=hover_color,
+            fg_color=button_color,
+            font=("arial", 20)
+        )
+        button.configure(command=lambda t=button_text: button_click(t))
+        button.pack(side=LEFT)
+
+root.mainloop()
