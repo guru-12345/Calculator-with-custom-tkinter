56af3316695e090d1954eed65be8bbb9061a04f3
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-17 16:19:35 +0530

    Create 5.2.1 lambda function.py

---
+++ 5.2.1 lambda function.py
@@ -0,0 +1,12 @@
+#Run and observe the output
+'''
+Why Use Lambda?
+1-> It’s a shortcut for small functions.
+2->No need to write def and return.
+3->Useful for quick, one-time calculations.'''
+
+#Task 1- Modify the given lambda function to triple a number instead of doubling it. 
+#Then, print the result for 5
+
+double = lambda x: x * 2  
+print(double(4))  
