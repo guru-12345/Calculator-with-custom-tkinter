89d6e24cfe59b7768defe1c3e1ac7412a1011676
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-17 21:59:57 +0530

    Update 5.3 Conditional Statements.py

--- 5.3 Conditional Statements.py
+++ 5.3 Conditional Statements.py
@@ -1,12 +1,13 @@
 # Run and observe the output.
-# Task 1 - Change the value of `num` to a number less than 5 and observe the output.
-# Task 2 - Change the value of num to exactly 5 and see what happens.
+#Task 1: Change num to -5 and observe the output.
+#Task 2: Change num to 0 and see what happens.
+#Task 3: Try using num = 10. What does the program print?
 
-num = 10  
+num = 7  
 
-if num > 5:  
-    print("The number is greater than 5")  
-elif num == 5:  
-    print("The number is exactly 5")  
+if num > 0:  
+    print("The number is Positive")  
+elif num == 0:  
+    print("The number is Zero")  
 else:  
-    print("The number is smaller than 5")  
+    print("The number is Negative")  
