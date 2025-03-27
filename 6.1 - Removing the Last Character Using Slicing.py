c1b191cdf9c8dc6e3c21732e8c3c4bdaaaf72e12
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-03-27 13:48:14 +0530

    Update 6.1 - Removing the Last Character Using Slicing.py

--- 6.1 - Removing the Last Character Using Slicing.py
+++ 6.1 - Removing the Last Character Using Slicing.py
@@ -1,8 +1,13 @@
-#Run and observe the output.
-#Task 1- Change -1 to -2 in both print statements and observe the output.
+# Run and observe the output.
+# Task 1- Change -1 to -2 in both print statements and observe the output.
 
 word = "Hello"
 print(word[:-1])  # Output: Hell (removes 'o')
 
 numbers = [1, 2, 3, 4, 5]
 print(numbers[:-1])  # Output: [1, 2, 3, 4] (removes 5)
+
+# Task 2 - Uncomment & Modify the slicing to remove the last two characters from the word and the last two numbers from the list.
+
+#print(word[:-2])  
+#print(numbers[:-2])  
