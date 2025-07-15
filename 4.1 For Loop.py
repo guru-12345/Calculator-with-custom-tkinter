d5224f4c8fa732e5c274300d3af511ec71396124
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-07-15 14:39:49 +0530

    Update 4.1 For Loop.py

--- 4.1 For Loop.py
+++ 4.1 For Loop.py
@@ -1,5 +1,8 @@
-#Task -1 Modify Activity-1 to print numbers from 10 to 20, including both 10 and 20.
-#Task -2 Create a list called 'my_subjects' with your favorite subjects and print each one using a for loop.
+#Task -1 Modify Activity-1 to print numbers from 10 to 20, 
+#including both 10 and 20.
+
+#Task -2 Create a list called 'my_subjects' with your 
+#favorite subjects and print each one using a for loop.
 
 #Activity -1
 for num in range(1, 6):  # range(1, 6) generates numbers from 1 to 5 (6 is not included)
